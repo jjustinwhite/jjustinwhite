da706cb1c2c4048f25a316ced1142299fda10df1
Grant Weiss <grant.sweiss@gmail.com>
2016-02-26 14:45:24 -0600

    Login logout and navbar functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 
-  get 'home'   => 'static_pages#home'
-  get 'signup' => 'users#new'
+  get    'home'    => 'static_pages#home'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	  end
 	  assert_template 'users/show'
 	  assert_not_nil flash
+	  assert is_logged_in?
   end
 end
