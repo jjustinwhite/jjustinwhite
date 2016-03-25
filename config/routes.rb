91bf3bf93e7230bb41761e2a314d99208348b38e
SirJustinWhite <Jjustinwhite@mac.com>
2016-03-24 22:23:42 -0500

    merge loginbase into app to make way for blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
-  root 'static_pages#home'
-
-  get    'home'    => 'static_pages#home'
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  root 'resume#index'
+  
+  get    'password_resets/new'
+  get    'password_resets/edit'
+  get    'sessions/new'
+  get    'resume'   => 'resume#index'
+  get    'projects' => 'projects#index'
+  get    'home'     => 'resume#index'
+  get    'signup'   => 'users#new'
+  get    'login'    => 'sessions#new'
+  post   'login'    => 'sessions#create'
+  delete 'logout'   => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
