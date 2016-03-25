c69fb395403b884ad619151525d5e679c3bd6a38
SirJustinWhite <Jjustinwhite@mac.com>
2016-03-24 22:35:45 -0500

    remove seeded sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,3 @@ User.create!(name: "Frank Zappa",
 						 admin: true,
 						 activated: true,
 						 activated_at: Time.zone.now)
-
-99.times do |n|
-	name = Faker::Name.name 
-	email = "example-#{n+1}@zappa.com"
-  password = "password"
-  User.create!(name: name,
-  						 email: email,
-  						 password: 						  password,
-  						 password_confirmation: password,
-  						 activated: true,
-  						 activated_at: Time.zone.now)
-end
