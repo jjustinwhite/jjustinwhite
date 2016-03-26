08c7301be55f9014b281371c2e16b6e9967335dd
SirJustinWhite <Jjustinwhite@mac.com>
2016-03-26 13:19:34 -0500

    change base title again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
-		base_title = "James Justin White III"
+		base_title = "Justin White"
 		if page_title.empty?
 			base_title
 		else
